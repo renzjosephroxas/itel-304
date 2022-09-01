eaf1c30304a6f4687c21b53e8e19c010485a3170
renzjosephroxas <101610321+renzjosephroxas@users.noreply.github.com>
2022-09-01 16:37:11 +0800

    Add files via upload

---
+++ activity 2/main.py
@@ -0,0 +1,4 @@
+addend1 = input('type first number: ')
+addend2 = input('type second number: ')
+sum = int(addend1) + int(addend2)
+print('the sum of {0} and {1} is {2}'. format(addend1, addend2, sum))
---
+++ activity 2/triagle area.py
@@ -0,0 +1,8 @@
+
+height = input('triagle height in cm: ')
+base = input('triagle base in cm: ')
+triagle_area = float(base) * float(height) // 2
+print('the area of this triagle is {0} cm'. format(triagle_area))
+
+
+
